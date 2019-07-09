684c8db519ad8a1312acb06d94b34be1f165d4d7
Frédéric <fpinaud17@gmail.com>
2019-07-09 17:18:50 +0200

    Added form to post data for digestive page

--- src/main/webapp/app/entities/symptomes/digestive/digestive.component.ts
+++ src/main/webapp/app/entities/symptomes/digestive/digestive.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DigestivesService } from 'app/entities/symptomes/digestive/digestives.service';
+import { Digestive } from 'app/shared/model/digestive';
+import { NgForm } from '@angular/forms';
+import { Account, AccountService } from 'app/core';
 
 @Component({
   selector: 'jhi-digestive',
@@ -6,7 +10,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./digestive.component.scss']
 })
 export class DigestiveComponent implements OnInit {
-  constructor() {}
+  digestive: Digestive;
+  account: Account;
 
-  ngOnInit() {}
+  constructor(private accountService: AccountService, protected digestiveService: DigestivesService) {}
+
+  ngOnInit() {
+    this.accountService.identity().then((account: Account) => {
+      this.account = account;
+    });
+  }
+
+  onSubmit(form: NgForm) {
+    this.digestive = new Digestive(
+      form.value.absenceSatiete,
+      form.value.aerophagie,
+      form.value.amaigrissement,
+      form.value.ballonements,
+      form.value.brulureEstomac,
+      form.value.commentaire,
+      form.value.constipation,
+      form.value.crampeIntestinale,
+      form.value.criseHemorroide,
+      form.value.douleurAbdominale,
+      form.value.douleurEpigastrique,
+      form.value.douleurVesiculaire,
+      form.value.dysphagie,
+      form.value.eructation,
+      form.value.estomacGonfle,
+      form.value.fausseRoute,
+      form.value.gastroParesie,
+      form.value.gaz,
+      form.value.hypoglycemieReactionnelle,
+      this.account.id,
+      form.value.imperiositeFecale,
+      form.value.impossibiliteAlimenter,
+      form.value.indigestion,
+      form.value.mauvaiseHaleine,
+      form.value.nausees,
+      form.value.perteAppetit,
+      form.value.pesanteurGastrique,
+      form.value.prisePoids,
+      form.value.regurgitation,
+      form.value.rgo,
+      form.value.saignementDigestifBas,
+      form.value.saignementDigestifHaut,
+      form.value.satietePrecoce,
+      form.value.selleLiquide,
+      form.value.sensationBrulureOesophage,
+      form.value.sensationFaimDouloureuse,
+      form.value.soifPermanente,
+      form.value.spasmeIntestinaux,
+      form.value.vomissements
+    );
+
+    this.digestiveService.postData(this.digestive).subscribe(
+      callback => {
+        console.log(callback);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
 }
--- src/main/webapp/app/entities/symptomes/digestive/digestives.service.ts
+++ src/main/webapp/app/entities/symptomes/digestive/digestives.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { SERVER_API_URL_MYSQL } from 'app/app.constants';
+import { Observable } from 'rxjs';
+import { Digestive } from 'app/shared/model/digestive';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/x-www-form-urlencoded'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class DigestivesService {
-  constructor() {}
+  public resourceUrl = SERVER_API_URL_MYSQL + '/api/manifestationDigestive';
+
+  constructor(protected http: HttpClient) {}
+
+  getData(): Observable<Digestive> {
+    return this.http.get<Digestive>(`${this.resourceUrl}`);
+  }
+
+  postData(digestive: Digestive): Observable<any> {
+    return this.http.post<Digestive>(`${this.resourceUrl}`, digestive);
+  }
 }
